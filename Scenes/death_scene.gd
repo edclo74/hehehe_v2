a0e0ae3a7809e637be6cfe31aecdb47f6030d515
edclo74 <edclo74@kingshigh.school.nz>
2024-09-19 12:24:53 +1200

    Added some neww things
    wedwew

--- Scenes/death_scene.gd
+++ Scenes/death_scene.gd
@@ -1,12 +1,12 @@
 extends Control
 @onready var gun = $Gun
 @onready var Score = $Death_screen_Score
+@onready var Music = $Musikness
 func _physics_process(delta):
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	gun.position = get_global_mouse_position()
 	gun.rotation_degrees = 45
-
-
+	Score.text = "Your Score Is " + str(Points.Points)
 
 
 
